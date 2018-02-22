820240a998504821f0152788f7073f17baa78292
Jake Zaia <jzaia@stuy.edu>
2018-02-22 18:49:24 -0500

    Finish implementing graphics matrix functionality

--- Draw.rb
+++ Draw.rb
@@ -13,24 +13,6 @@ module Draw
     return board
   end
 
-  ## Write GRID to OUTFILE
-  def self.write_out(file)
-    file = File.open(file, 'w')
-    file.puts "P3 #$RESOLUTION #$RESOLUTION 255" #Header in 1 line
-    for row in $GRID
-      for pixel in row
-        for rgb in pixel
-          file.print rgb
-          file.print ' '
-        end
-        file.print '   '
-      end
-      file.puts ''
-    end
-    file.close()
-  end
-
-
   # Plot a point on GRID (from top left)
   def self.plot(x, y, r: 255, g: 255, b: 255) $GRID[y%$RESOLUTION][x%$RESOLUTION] = [r.floor, g.floor, b.floor] end
   # Plot a point on GRID (from bottom left)
@@ -39,139 +21,67 @@ module Draw
   # Define a line by 2 points
   def self.line(x0, y0, x1, y1, r: 255, g: 255, b: 255)
     # x0 is always left of x1
-    if x1 < x0; line(x1, y1, x0, y0, r: r, g: g, b: b) else
+    line(x1, y1, x0, y0, r: r, g: g, b: b) if x1 < x0
 
-      #init vars
-      dy = y1-y0
-      dx = x1-x0
-      x = x0
-      y = y0
-      d = 2*dy - dx
+    #init vars
+    dy = y1-y0
+    dx = x1-x0
+    x = x0
+    y = y0
+    d = -1 * dx
 
-      ## Begin actual algorithm:
-      if dy <= 0 #if the line is in octants I or II
-        if dy.abs <= dx #octant I
-          puts "Drawing line in Octant I" if $DEBUGGING
-          while x <= x1
-            plot(x, y, r: r, g: g, b: b)
-            if d > 0
-              y-=1
-              d-=2*dx
-            end
-            x+=1
-            d-=2*dy
-          end
-
-        else #octant II
-          puts "Drawing line in Octant II" if $DEBUGGING
-          while y >= y1
-            plot(x, y, r: r, g: g, b: b)
-            if d > 0
-              x+=1
-              d+=2*dy
-            end
+    ## Begin actual algorithm:
+    if dy <= 0 #if the line is in octants I or II
+      if dy.abs <= dx #octant I
+        puts "Drawing line in Octant I" if $DEBUGGING
+        while x <= x1
+          plot(x, y, r: r, g: g, b: b)
+          if d > 0
             y-=1
-            d+=2*dx
+            d-=2*dx
           end
+          x+=1
+          d-=2*dy
         end
 
-      else #if the line is in octants VII or VIII
-
-        if dy >= dx #octant VII
-          puts "Drawing line in Octant VII" if $DEBUGGING
-          while y <= y1
-            plot(x, y, r: r, g: g, b: b)
-            if d > 0
-              x+=1
-              d-=2*dy
-            end
-            y+=1
-            d+=2*dx
-          end
-
-        else #octant VIII
-          puts "Drawing line in Octant VIII" if $DEBUGGING
-          while x <= x1
-            plot(x, y, r: r, g: g, b: b)
-            if d > 0
-              y+=1
-              d-=2*dx
-            end
+      else #octant II
+        puts "Drawing line in Octant II" if $DEBUGGING
+        while y >= y1
+          plot(x, y, r: r, g: g, b: b)
+          if d > 0
             x+=1
             d+=2*dy
           end
-        end
-      end
-    end
-  end
-
-  ## Uses 1 while loop. It's actually slower than the other line function.
-  def self.lineINTEGRATED(x0, y0, x1, y1, r: 255, g: 255, b: 255)
-    # x0 is always left of x1
-    if x1 < x0; line(x1, y1, x0, y0, r: r, g: g, b: b) else
-
-      #init vars
-      dy = y1 - y0
-      dx = x1 - x0
-
-      # incrvar = nil    #either x or y, constantly changes by 1
-      # dynamicvar = nil #either x or y, may or may not change
-      # stoppoint = nil  #the point to stop at (incrvar's counterpart)
-      # incrd = nil      #what to increment d by each iteration
-      # dynamicd = nil   #what to increment d by on occaision
-      # octant = nil     #the octant the line is in
-
-      d = 2 * dy - dx
-
-      if dy<0
-        if dy.abs < dx;
-          puts "Drawing line in Octant I" if $DEBUGGING
-          octant = 1
-          incrvar = x0
-          dynamicvar = y0
-          incrd = -2 * dy
-          dynamicd = -2 * dx
-          stoppoint = x1
-        else
-          puts "Drawing line in Octant II" if $DEBUGGING
-          octant = 2
-          incrvar = y0
-          dynamicvar = x0
-          incrd = 2 * dx
-          dynamicd = 2 * dy
-          stoppoint = y1
-        end
-      else
-        if dy > dx;
-          puts "Drawing line in Octant VII" if $DEBUGGING
-          octant = 7
-          incrvar = y0
-          dynamicvar = x0
-          incrd = 2 * dx
-          dynamicd = -2 * dy
-          stoppoint = y1
-        else
-          puts "Drawing line in Octant VIII" if $DEBUGGING
-          octant = 8
-          incrvar = x0
-          dynamicvar = y0
-          incrd = 2 * dy
-          dynamicd = -2 * dx
-          stoppoint = x1
+          y-=1
+          d+=2*dx
         end
       end
 
-      while (incrvar - stoppoint).abs > 0
-        plot(incrvar, dynamicvar, r: r, g: g, b: b) if octant == 1 or octant == 8
-        plot(dynamicvar, incrvar, r: r, g: g, b: b) if octant == 2 or octant == 7
-        if d > 0
-          if octant == 1; dynamicvar-= 1 else
-            dynamicvar += 1 end
-          d+= dynamicd
+    else #if the line is in octants VII or VIII
+
+      if dy >= dx #octant VII
+        puts "Drawing line in Octant VII" if $DEBUGGING
+        while y <= y1
+          plot(x, y, r: r, g: g, b: b)
+          if d > 0
+            x+=1
+            d-=2*dy
+          end
+          y+=1
+          d+=2*dx
         end
-        if octant == 2; incrvar -= 1 else
-          incrvar += 1 end
-        d+= incrd
+
+      else #octant VIII
+        puts "Drawing line in Octant VIII" if $DEBUGGING
+        while x <= x1
+          plot(x, y, r: r, g: g, b: b)
+          if d > 0
+            y+=1
+            d-=2*dx
+          end
+          x+=1
+          d+=2*dy
+        end
       end
     end
   end
@@ -190,44 +100,27 @@ module Draw
     line(x0, y0, x1, y1, r: r, g: g, b: b)
   end
 
-  ##========================== MAIN ==========================
+  # Helper for add_edge
+  def self.add_point(x, y, z)
+    $EDGE_MAT.add_col([x, y, z, 1])
+  end
 
-  ##~~~~~~~~~~~~~~~~~~~ Test cases ~~~~~~~~~~~~~~~~~~~
-  #  line(250, 250, 499, 240, r:0) #Octant I
-  #  line(250, 250, 499, 200, r:0) #Octant I
-  #  line(250, 250, 499, 150, r:0) #Octant I
-  #  line(250, 250, 260, 0, g:0) #Octant II
-  #  line(250, 250, 350, 0, g:0) #Octant II
-  #  line(250, 250, 400, 0, g:0) #Octant II
-  #  line(250, 250, 260, 499, b:0) #Octant VII
-  #  line(250, 250, 290, 499, b:0) #Octant VII
-  #  line(250, 250, 400, 499, b:0) #Octant VII
-  #  line(250, 250, 499, 275) #Octant VIII
-  #  line(250, 250, 499, 400) #Octant VIII
-  #  line(250, 250, 499, 450) #Octant VIII
+  # Add an edge to the global edge matrix
+  def self.add_edge(x0, y0, z0, x1, y1, z1)
+    add_point(x0, y0, z0)
+    add_point(x1, y1, z1)
+  end
 
-  # ## literal edge cases
-  #  line(0, 499, 499, 0, r:0) #Octant II edge
-  #  line(250, 499, 250, 0, g:0) #Octant II edge
-  #  line(0, 0, 499, 499, b:0) #Octant VIII edge
-  #  line(0, 250, 499, 250) #Octant VIII edge
-
-  ##~~~~~~~~~~~~~~~~~~~ Cool Image to show in Gallery ~~~~~~~~~~~~~~~~~~~
-
-  # # Draw a circle
-  # i = 0
-  # while i < TAU
-  #   line_directed(250, 250, i, 250, r: i*100, g: i*120, b: i*150)
-  #   i+= 0.01
-  # end
-
-  # # Hollow out the center
-  # i = 0
-  # while i < TAU
-  #   line_directed(250, 250, i, 75, r: 0, g: 0, b: 0)
-  #   i+= 0.00125 # Needs more precision
-  # end
-
-  # write_out()
+  # Draw the pixels in the matrix and clean it out
+  def self.push_edge_matrix()
+    i = 0
+    while i < $EDGE_MAT.cols
+      coord0 = $EDGE_MAT.get_col(i)
+      coord1 = $EDGE_MAT.get_col(i + 1)
+      line(coord0[0], coord0[1], coord1[0], coord1[1])
+      i+=2
+    end
+    $EDGE_MAT = Matrix.new(4, 0)
+  end
 
 end
--- Main.rb
+++ Main.rb
@@ -11,41 +11,74 @@ $RESOLUTION = 500 # All images are squares
 $DEBUGGING = false
 $BACKGROUND_COLOR = [0, 0, 0] # [r, g, b]
 $OUTFILE = "image.ppm"
-#$GRID = Draw.create_board()
+$GRID = Draw.create_board()
+$EDGE_MAT = Matrix.new(4, 0) # An edge matrix
 
-# # Draw a circle
-# i = 0
-# while i < $TAU
-#   Utils.line_directed(250, 250, i, 250, r: i*100, g: i*120, b: i*150)
-#   i+= 0.01
-# end
 
-# Utils.write_out($OUTFILE)
+## Write GRID to OUTFILE
+def self.write_out(file)
+  file = File.open(file, 'w')
+  file.puts "P3 #$RESOLUTION #$RESOLUTION 255" #Header in 1 line
+  for row in $GRID
+    for pixel in row
+      for rgb in pixel
+        file.print rgb
+        file.print ' '
+      end
+      file.print '   '
+    end
+    file.puts ''
+  end
+  file.close()
+end
 
-edge_mat = Matrix.new(4, 0)
+##========================== MAIN ==========================
 
-edge_mat.set(2, 5, 3)
 
-puts edge_mat
+## TEST matrix functionality
+mat = Matrix.new(4, 0)
 
-edge_mat.add_col([1, 2, 3, 4])
-edge_mat.add_col([1, 2, 3, 4])
-edge_mat.add_col([1, 2, 3, 4])
-edge_mat.add_col([1, 2, 3, 4])
-edge_mat.add_col([1, 2, 3, 4])
-edge_mat.add_col([1, 2, 3, 4])
+puts mat
 
-puts edge_mat
+mat.add_col([1, 2, 3, 4])
+mat.add_col([1, 2, 3, 4])
+mat.add_col([1, 2, 3, 4])
+mat.add_col([1, 2, 3, 4])
+mat.add_col([1, 2, 3, 4])
+mat.add_col([1, 2, 3, 4])
+
+puts "Test matrix"
+puts mat
 
 a = MatrixUtils.identity(4)
 
+puts "Test identity matrix"
 puts a
 
+puts "Get 2 specific indices:"
 puts a.get(1,1)
 puts a.get(1,0)
+puts "Get a row"
 puts a.get_row(1).to_s
+puts "Get a collumn"
 puts a.get_col(1).to_s
 
-puts MatrixUtils.multiply(a, edge_mat)
-puts MatrixUtils.add(edge_mat, edge_mat)
-puts MatrixUtils.subtract(edge_mat, edge_mat)
+puts "\nMultiplying by the identity..."
+puts MatrixUtils.multiply(a, mat)
+puts "\nAdding it to itself..."
+puts MatrixUtils.add(mat, mat)
+puts "\nSubtracting it from itself..."
+puts MatrixUtils.subtract(mat, mat)
+
+
+## TEST edge matrices
+
+i = 0
+while i <= $RESOLUTION
+  Draw.add_edge(i, 0, 0, i, $RESOLUTION, 0)
+  Draw.add_edge(0, i, 0, $RESOLUTION, i, 0)
+  i += 20
+end
+
+Draw.push_edge_matrix()
+write_out($OUTFILE)
